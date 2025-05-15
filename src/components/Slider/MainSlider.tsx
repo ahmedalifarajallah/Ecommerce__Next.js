7fb23a841d1edb4fffb33e8a5a38c9dcd2905a5a
ahmedalifarajallah <ahmedfarajallah00@gmail.com>
2025-05-15 17:19:31 +0300

    .

--- src/components/Slider/MainSlider.tsx
+++ src/components/Slider/MainSlider.tsx
@@ -70,6 +70,7 @@ const MainSlider = () => {
                   src={slide.imageSrc}
                   alt={slide.title}
                   fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   className="object-cover"
                   priority={index === 0}
                 />
